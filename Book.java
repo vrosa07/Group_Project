3047aff8fa7899522102558e27c410591325f656
vrosa07 <102338363+vrosa07@users.noreply.github.com>
2023-11-27 18:05:57 -0600

    Add files via upload
    I've uploaded the working version of this project. This is the final upload.

--- Book.java
+++ Book.java
@@ -1,32 +1,34 @@
 
 
 public class Book {
-    String title;
-    boolean borrowed;
+    private String title;
+    private boolean borrowed;
 
     // Creates a new Book
     public Book(String bookTitle) {
-        // Implement this method
+        this.title = bookTitle;
+        this.borrowed = false;
+
     }
 
     // Marks the book as rented
-    public void borrowed() {
-        // Implement this method
+    public void rented() {
+        this.borrowed = true;
     }
 
     // Marks the book as not rented
     public void returned() {
-        // Implement this method
+        this.borrowed = false;
     }
 
     // Returns true if the book is rented, false otherwise
     public boolean isBorrowed() {
-        // Implement this method
+        return this.borrowed;
     }
 
     // Returns the title of the book
     public String getTitle() {
-        // Implement this method
+        return this.title;
     }
 
     public static void main(String[] arguments) {
--- Library.java
+++ Library.java
@@ -1,18 +1,118 @@
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.File;
+import java.io.*;
 
 public class Library {
-    // Add the missing implementation to this class
+    private static String openingHours = "9 AM to 5 PM";
+    private String address;
+    private ArrayList<Book> books = new ArrayList<>();; // Collection of books in the library
+    private HashMap<String, Integer> copiesOfBooks = new HashMap<>();
+
+    // Constructor for a library without a catalog file
+    public Library(String address) {
+        this.address = address;
+    }
+
+    // Constructor for a library with a catalog file
+    public Library(String address, String catalogFile) {
+        this.address = address;
+        importBooksFromCatalog(catalogFile);
+    }
+
+    // Add a book to the library
+    public void addBook(Book book) {
+        if (copiesOfBooks.get(book.getTitle ()) == null){
+            books.add(book);
+            copiesOfBooks.put(book.getTitle(), 1);
+        }else {
+           copiesOfBooks.put(book.getTitle(), copiesOfBooks.get(book.getTitle()) + 1);
+        }
+    }
+
+    // Borrow a book from the library
+    public void borrowBook(String title) {
+
+            if (copiesOfBooks.get(title) == null) {
+                System.out.println ("We do not have this book in the catalog");
+            } else {
+                if (copiesOfBooks.get(title) == 0) {
+                    System.out.println("Books is in catalog, but all copies are borrowed.");
+                } else {
+                    copiesOfBooks.put(title, copiesOfBooks.get(title) - 1);
+                    System.out.println("You successfully borrowed " + title +
+                            ", remaining number of copies: " + copiesOfBooks.get(title));
+                }
+            }
+    }
+
+    // Return a book to the library
+    public void returnBook(String title) {
+        if (copiesOfBooks.get(title) == null) {
+            System.out.println ("We do not have this book in the catalog");
+        } else {
+
+                copiesOfBooks.put(title, copiesOfBooks.get(title) + 1);
+                System.out.println("You successfully returned " + title +
+                        ", remaining number of copies: " + copiesOfBooks.get(title));
+
+        }
+    }
+
+    // Print available books in the library
+    public void printAvailableBooks() {
+        for (Book book : books) {
+            System.out.println(book.getTitle() + ", remaining number of copies: "
+                    + getRemainingCopies(book.getTitle()));
+        }
+    }
+
+    // Print the library's opening hours
+    public static void printOpeningHours() {
+        System.out.println(openingHours);
+    }
+
+    // Print the library's address
+    public void printAddress() {
+        System.out.println(address);
+    }
+
+    // Import books from a catalog file (if provided)
+    private void importBooksFromCatalog(String catalogFile) {
+        try {
+            File file = new File("catalog.csv");
+            BufferedReader b = new BufferedReader(new FileReader(file));
+            String st;
+            while((st = b.readLine()) != null){
+                String[] splitString = st.split("[,]", 0);
+                String title = splitString[0];
+                Integer numberOfCopies = Integer.valueOf(splitString[1]);
+                for(int i = 0; i < numberOfCopies; i++){
+                    addBook(new Book(title));
+                }
+
+            }
+        } catch (Exception e) {
+            System.out.println("File not found.");
+        }
+    }
+
+
+    // Get the remaining number of copies for a specific book
+    private int getRemainingCopies(String title) {
+        return copiesOfBooks.get(title);
+    }
 
     public static void main(String[] args) {
         // Create two libraries
         Library firstLibrary = new Library("10 Main St.");
         Library secondLibrary = new Library("228 Liberty St.");
-        Library thirdLibrary = new Library("12 Broadway St.", "catalog.txt");
+        Library thirdLibrary = new Library("12 Broadway St.", "catalog.csv");
 
         // Add four books to the first library
         firstLibrary.addBook(new Book("The Da Vinci Code"));
         firstLibrary.addBook(new Book("The Da Vinci Code")); // second copy
-        firstLibrary.addBook(new Book("Le Petit Prince"));
+        firstLibrary.addBook(new Book("Le Petite Prince"));
         firstLibrary.addBook(new Book("A Tale of Two Cities"));
         firstLibrary.addBook(new Book("The Lord of the Rings"));
         firstLibrary.addBook(new Book("The Lord of the Rings")); // second copy
@@ -28,7 +128,7 @@ public class Library {
         thirdLibrary.printAddress();
         System.out.println();
 
-        // Try to borrow The Lords of the Rings from both libraries
+        // Try to borrow The Lord of the Rings from both libraries
         System.out.println("Borrowing The Lord of the Rings:");
         firstLibrary.borrowBook("The Lord of the Rings");
         firstLibrary.borrowBook("The Lord of the Rings");
@@ -40,13 +140,13 @@ public class Library {
         System.out.println("Books available in the first library:");
         firstLibrary.printAvailableBooks();
         System.out.println();
-        System.out.println("Books available in the second library:");
+        System.out.println("Books available in the second library: 0\n");
         secondLibrary.printAvailableBooks();
         System.out.println("Books available in the third library:");
         thirdLibrary.printAvailableBooks();
         System.out.println();
 
-        // Return The Lords of the Rings to the first library
+        // Return The Lord of the Rings to the first library
         System.out.println("Returning The Lord of the Rings:");
         firstLibrary.returnBook("The Lord of the Rings");
         System.out.println();
